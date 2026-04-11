e5766dc32affcb1bd0e4a070c03a9234f6ca5656
Slerne <73561281+NFerrovia@users.noreply.github.com>
2026-04-10 23:38:07 -0300

    Add files via upload
    Fixed UI being bottom layer to Blizzard's UI

--- ViewerUI.lua
+++ ViewerUI.lua
@@ -3,6 +3,7 @@ local frame = SlerneNotesViewer.frame
 
 frame:SetSize(1600, 950)
 frame:SetPoint("CENTER")
+frame:SetFrameStrata("FULLSCREEN_DIALOG") -- Forces the viewer to the top layer
 frame:EnableMouse(true)
 frame:SetMovable(true)
 frame:RegisterForDrag("LeftButton")
